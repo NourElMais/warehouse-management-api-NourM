7206bfe4d4fd3316f471e49a65ba4ad98d3e1a3e
Nour <yourEmail@example.com>
2026-07-01 15:22:37 +0300

    test(session-02):Created the Contracts Folder with 3 entities inside

---
+++ warehouse-management-api-NourM/warehouse-management-api-NourM/Contracts/UpdateProductPriceRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace warehouse_management_api_NourM.Contracts;
+
+public class UpdateProductPriceRequest
+{
+    public int Price {get; set;}
+}
---
+++ warehouse-management-api-NourM/warehouse-management-api-NourM/Contracts/UpdateProductQuantityRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace warehouse_management_api_NourM.Contracts;
+
+public class UpdateProductQuantityRequest
+{
+    public int QuantityInStock { get; set; }
+}

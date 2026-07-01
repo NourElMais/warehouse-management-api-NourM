8dc99a2c8a0cdf2111a69dc9d969a46e561462b8
Nour <yourEmail@example.com>
2026-07-01 15:18:56 +0300

    test(session-02):Created the Models folder with the 2 classes inside (Product.cs and ProductImage.cs)

---
+++ warehouse-management-api-NourM/warehouse-management-api-NourM/Models/ProductImage.cs
@@ -0,0 +1,8 @@
+﻿namespace warehouse_management_api_NourM.Models;
+
+public class ProductImage
+{
+    public string ProductId { get; set; }
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+}

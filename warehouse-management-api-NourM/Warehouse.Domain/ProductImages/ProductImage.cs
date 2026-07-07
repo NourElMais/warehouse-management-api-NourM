c5a884a91a1fd774e2145f79c52e9e8a4cd2471b
Nour <yourEmail@example.com>
2026-07-07 12:06:30 +0300

    feat(session-03): Created the ProductImage.cs file inside the domain layer

---
+++ warehouse-management-api-NourM/Warehouse.Domain/ProductImages/ProductImage.cs
@@ -0,0 +1,27 @@
+﻿namespace Warehouse.Domain.ProductImages;
+
+public class ProductImage
+{
+    public string ProductId { get; private set; }
+    public string FileName { get; private set; }
+    public string FilePath { get; private set; }
+
+    public ProductImage(
+        string productId,
+        string fileName,
+        string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(productId))
+            throw new ArgumentException("Product Id is required.");
+
+        if (string.IsNullOrWhiteSpace(fileName))
+            throw new ArgumentException("File name is required.");
+
+        if (string.IsNullOrWhiteSpace(filePath))
+            throw new ArgumentException("File path is required.");
+
+        ProductId = productId;
+        FileName = fileName;
+        FilePath = filePath;
+    }
+}

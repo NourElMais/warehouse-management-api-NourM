1ab54d90e5937cad873f54602c4c83c1ce7cead3
Nour <yourEmail@example.com>
2026-07-07 14:32:26 +0300

    feat(session-03): Created the AssignSupplierToProduct command and handler

---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Commands/AssignSupplierToProductCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Warehouse.Application.Products.Commands;
+
+public class AssignSupplierToProductCommand
+{
+    public string ProductId { get; set; }
+    public string SupplierId { get; set; }
+
+    public AssignSupplierToProductCommand(string productId, string supplierId)
+    {
+        ProductId = productId;
+        SupplierId = supplierId;
+    }
+}
---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Commands/AssignSupplierToProductHandler.cs
@@ -0,0 +1,37 @@
+﻿using Warehouse.Domain.Products;
+using Warehouse.Domain.Repositories;
+
+namespace Warehouse.Application.Products.Commands;
+
+public class AssignSupplierToProductHandler
+{
+    private readonly IProductRepository _productRepository;
+    private readonly ISupplierRepository _supplierRepository;
+
+    public AssignSupplierToProductHandler(
+        IProductRepository productRepository,
+        ISupplierRepository supplierRepository)
+    {
+        _productRepository = productRepository;
+        _supplierRepository = supplierRepository;
+    }
+
+    public Product? Handle(AssignSupplierToProductCommand command)
+    {
+        var product = _productRepository.GetById(command.ProductId);
+
+        if (product is null)
+            return null;
+
+        var supplier = _supplierRepository.GetById(command.SupplierId);
+
+        if (supplier is null)
+            return null;
+
+        product.AssignSupplier(supplier.Name, supplier.IsActive);
+
+        _productRepository.Update(product);
+
+        return product;
+    }
+}

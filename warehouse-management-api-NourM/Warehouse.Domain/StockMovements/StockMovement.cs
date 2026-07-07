3f46982f78280c6007bb5a59218c7adfd6faead3
Nour <yourEmail@example.com>
2026-07-07 12:12:40 +0300

    feat(session-03): Created the StockMovement class inside the domain layer

---
+++ warehouse-management-api-NourM/Warehouse.Domain/StockMovements/StockMovement.cs
@@ -0,0 +1,21 @@
+﻿namespace Warehouse.Domain.StockMovements;
+
+public class StockMovement
+{
+    public string ProductId { get; private set; }
+    public int QuantityChanged { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public StockMovement(string productId, int quantityChanged)
+    {
+        if (string.IsNullOrWhiteSpace(productId))
+            throw new ArgumentException("Product Id is required.");
+
+        if (quantityChanged == 0)
+            throw new ArgumentException("Quantity changed cannot be zero.");
+
+        ProductId = productId;
+        QuantityChanged = quantityChanged;
+        CreatedAt = DateTime.UtcNow;
+    }
+}

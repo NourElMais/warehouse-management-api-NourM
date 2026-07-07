023fc93593e90f220f8d4e7027ecbec8d7fe9b1d
Nour <yourEmail@example.com>
2026-07-07 15:02:13 +0300

    feat(session-03): Created the CreateSupplier command and handler

---
+++ warehouse-management-api-NourM/Warehouse.Application/Suppliers/Commands/CreateSupplierCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Warehouse.Application.Suppliers.Commands;
+
+public class CreateSupplierCommand
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string ContactEmail { get; set; }
+    public string PhoneNumber { get; set; }
+}
---
+++ warehouse-management-api-NourM/Warehouse.Application/Suppliers/Commands/CreateSupplierHandler.cs
@@ -0,0 +1,28 @@
+﻿using Warehouse.Domain.Repositories;
+using Warehouse.Domain.Suppliers;
+
+namespace Warehouse.Application.Suppliers.Commands;
+
+public class CreateSupplierHandler
+{
+    private readonly ISupplierRepository _supplierRepository;
+
+    public CreateSupplierHandler(ISupplierRepository supplierRepository)
+    {
+        _supplierRepository = supplierRepository;
+    }
+
+    public Supplier Handle(CreateSupplierCommand command)
+    {
+        var supplier = new Supplier(
+            command.Name,
+            command.Country,
+            command.ContactEmail,
+            command.PhoneNumber
+        );
+
+        _supplierRepository.Add(supplier);
+
+        return supplier;
+    }
+}

a0d223bf0addb97c3eb25a6f66239bf42aba84e3
Nour <yourEmail@example.com>
2026-07-07 12:11:36 +0300

    feat(session-03): Created the WareHouseItem class inside the domain layer

---
+++ warehouse-management-api-NourM/Warehouse.Domain/WareHouseItems/WareHouseItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Warehouse.Domain.WareHouseItems;
+
+public class WareHouseItem
+{
+    public string ProductId { get; private set; }
+    public int Quantity { get; private set; }
+
+    public WareHouseItem(string productId, int quantity)
+    {
+        if (string.IsNullOrWhiteSpace(productId))
+            throw new ArgumentException("Product Id is required.");
+
+        if (quantity < 0)
+            throw new ArgumentException("Quantity cannot be negative.");
+
+        ProductId = productId;
+        Quantity = quantity;
+    }
+}

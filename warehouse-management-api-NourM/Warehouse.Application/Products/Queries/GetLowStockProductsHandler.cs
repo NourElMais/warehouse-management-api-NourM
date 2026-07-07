10121371979e585a68f8f9f2d9bb10d1d42a0e6f
Nour <yourEmail@example.com>
2026-07-07 14:54:28 +0300

    feat(session-03): Created the GetLowStockProducts command and handler (exra endpoint that was not mentioned in the lab

---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Queries/GetLowStockProductsHandler.cs
@@ -0,0 +1,22 @@
+﻿using Warehouse.Domain.Products;
+using Warehouse.Domain.Repositories;
+
+namespace Warehouse.Application.Products.Queries;
+
+public class GetLowStockProductsHandler
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetLowStockProductsHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public List<Product> Handle(GetLowStockProductsQuery query)
+    {
+        return _productRepository
+            .GetAll()
+            .Where(p => !p.IsArchived && p.QuantityInStock <= query.Threshold)
+            .ToList();
+    }
+}
---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Queries/GetLowStockProductsQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace Warehouse.Application.Products.Queries;
+
+public class GetLowStockProductsQuery
+{
+    public int Threshold { get; set; }
+
+    public GetLowStockProductsQuery(int threshold)
+    {
+        Threshold = threshold;
+    }
+}

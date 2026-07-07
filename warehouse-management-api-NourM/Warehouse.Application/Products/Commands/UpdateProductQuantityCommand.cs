464dad517d5a68682e7fc4d7aef439c364cfc9dc
Nour <yourEmail@example.com>
2026-07-07 14:20:37 +0300

    feat(session-03): Created the UpdateProductQuantityCommand and handler

---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Commands/UpdateProductQuantityCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Warehouse.Application.Products.Commands;
+
+public class UpdateProductQuantityCommand
+{
+    public string ProductId { get; set; }
+    public int NewQuantity { get; set; }
+
+    public UpdateProductQuantityCommand(string productId, int newQuantity)
+    {
+        ProductId = productId;
+        NewQuantity = newQuantity;
+    }
+}
---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Commands/UpdateProductQuantityHandler.cs
@@ -0,0 +1,28 @@
+﻿using Warehouse.Domain.Products;
+using Warehouse.Domain.Repositories;
+
+namespace Warehouse.Application.Products.Commands;
+
+public class UpdateProductQuantityHandler
+{
+    private readonly IProductRepository _productRepository;
+
+    public UpdateProductQuantityHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public Product? Handle(UpdateProductQuantityCommand command)
+    {
+        var product = _productRepository.GetById(command.ProductId);
+
+        if (product is null)
+            return null;
+
+        product.UpdateQuantity(command.NewQuantity);
+
+        _productRepository.Update(product);
+
+        return product;
+    }
+}

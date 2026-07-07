2d443c210c1bf4fd8c56123be9b938e0a246ecce
Nour <yourEmail@example.com>
2026-07-07 14:24:01 +0300

    feat(session-03): Created the UpdateProductPriceCommand and handler

---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Commands/UpdateProductPriceCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Warehouse.Application.Products.Commands;
+
+public class UpdateProductPriceCommand
+{
+    public string ProductId { get; set; }
+    public decimal NewPrice { get; set; }
+
+    public UpdateProductPriceCommand(string productId, decimal newPrice)
+    {
+        ProductId = productId;
+        NewPrice = newPrice;
+    }
+}
---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Commands/UpdateProductPriceHandler.cs
@@ -0,0 +1,28 @@
+﻿using Warehouse.Domain.Products;
+using Warehouse.Domain.Repositories;
+
+namespace Warehouse.Application.Products.Commands;
+
+public class UpdateProductPriceHandler
+{
+    private readonly IProductRepository _productRepository;
+
+    public UpdateProductPriceHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public Product? Handle(UpdateProductPriceCommand command)
+    {
+        var product = _productRepository.GetById(command.ProductId);
+
+        if (product is null)
+            return null;
+
+        product.UpdatePrice(command.NewPrice);
+
+        _productRepository.Update(product);
+
+        return product;
+    }
+}

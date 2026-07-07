3e5d48a4ec1275774bb750fe17867ee49cea4fd1
Nour <yourEmail@example.com>
2026-07-07 14:28:35 +0300

    feat(session-03): Created the ArchiveProduct command and handler

---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Commands/ArchiveProductCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Warehouse.Application.Products.Commands;
+
+public class ArchiveProductCommand
+{
+    public string ProductId { get; set; }
+
+    public ArchiveProductCommand(string productId)
+    {
+        ProductId = productId;
+    }
+}
---
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Commands/ArchiveProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using Warehouse.Domain.Products;
+using Warehouse.Domain.Repositories;
+
+namespace Warehouse.Application.Products.Commands;
+
+public class ArchiveProductHandler
+{
+    private readonly IProductRepository _productRepository;
+
+    public ArchiveProductHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public Product? Handle(ArchiveProductCommand command)
+    {
+        var product = _productRepository.GetById(command.ProductId);
+
+        if (product is null)
+            return null;
+
+        product.Archive();
+
+        _productRepository.Update(product);
+
+        return product;
+    }
+}

2b26b432aaa0a5fc21bcc1b4236948a55fa82365
Nour <yourEmail@example.com>
2026-07-07 12:26:39 +0300

    feat(session-03): Created the Repositories folder in the domain layer with 2 files inside (IProductRepository and ISupplierRepository)

---
+++ warehouse-management-api-NourM/Warehouse.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Warehouse.Domain.Products;
+
+namespace Warehouse.Domain.Repositories;
+
+public class IProductRepository
+{
+    List<Product> GetAll();
+    Product? GetById(string id);
+    List<Product> Search(string searchTerm);
+    void Add(Product product);
+    void Update(Product product);
+}
---
+++ warehouse-management-api-NourM/Warehouse.Domain/Repositories/ISupplierRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Warehouse.Domain.Repositories;
+
+public class ISupplierRepository
+{
+    List<Supplier> GetAll();
+    Supplier? GetById(string id);
+    void Add(Supplier supplier);
+    void Update(Supplier supplier);
+}

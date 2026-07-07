c7863536aab166a3d2594436f54a5ef1382856ac
Nour <yourEmail@example.com>
2026-07-07 15:06:22 +0300

    feat(session-03): Created the DeactivateSupplier command and handler

---
+++ warehouse-management-api-NourM/Warehouse.Application/Suppliers/Commands/DeactivateSupplierCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Warehouse.Application.Suppliers.Commands;
+
+public class DeactivateSupplierCommand
+{
+    public string SupplierId { get; set; }
+
+    public DeactivateSupplierCommand(string supplierId)
+    {
+        SupplierId = supplierId;
+    }
+}
---
+++ warehouse-management-api-NourM/Warehouse.Application/Suppliers/Commands/DeactivateSupplierHandler.cs
@@ -0,0 +1,28 @@
+﻿using Warehouse.Domain.Repositories;
+using Warehouse.Domain.Suppliers;
+
+namespace Warehouse.Application.Suppliers.Commands;
+
+public class DeactivateSupplierHandler
+{
+    private readonly ISupplierRepository _supplierRepository;
+
+    public DeactivateSupplierHandler(ISupplierRepository supplierRepository)
+    {
+        _supplierRepository = supplierRepository;
+    }
+
+    public Supplier? Handle(DeactivateSupplierCommand command)
+    {
+        var supplier = _supplierRepository.GetById(command.SupplierId);
+
+        if (supplier is null)
+            return null;
+
+        supplier.Deactivate();
+
+        _supplierRepository.Update(supplier);
+
+        return supplier;
+    }
+}

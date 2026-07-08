6eafa5837fcd23e5b777b4e9386efa5b83670930
Nour <yourEmail@example.com>
2026-07-08 10:17:01 +0300

    docs(session-03): Added screenshots to the screenshots folder

--- warehouse-management-api-NourM/Warehouse.Application/Products/Queries/GetServerTimeHandler.cs
+++ warehouse-management-api-NourM/Warehouse.Application/Products/Queries/GetServerTimeHandler.cs
@@ -1,13 +1,41 @@
-﻿using MediatR;
+﻿using System.Globalization;
+using MediatR;
 
 namespace Warehouse.Application.Products.Queries;
 
 public class GetServerTimeHandler : IRequestHandler<GetServerTimeQuery, string>
 {
-    public Task<string> Handle(GetServerTimeQuery request, CancellationToken cancellationToken)
+    public Task<string> Handle(
+        GetServerTimeQuery request,
+        CancellationToken cancellationToken)
     {
-        string time = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+        DateTime now = DateTime.UtcNow;
 
-        return Task.FromResult($"Server time: {time}");
+        string result;
+
+        if (request.Language == "en-US")
+        {
+            result = now.ToString(
+                "MMMM dd, yyyy hh:mm:ss tt",
+                new CultureInfo("en-US"));
+        }
+        else if (request.Language == "fr-FR")
+        {
+            result = now.ToString(
+                "dd MMMM yyyy HH:mm:ss",
+                new CultureInfo("fr-FR"));
+        }
+        else if (request.Language == "ar-LB")
+        {
+            result = now.ToString(
+                "dd MMMM yyyy HH:mm:ss",
+                new CultureInfo("ar-LB"));
+        }
+        else
+        {
+            result = now.ToString();
+        }
+
+        return Task.FromResult(result);
     }
 }

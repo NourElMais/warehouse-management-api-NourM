8d4a73f36110af00e4fadd70c73789fce7c1dccb
Nour <yourEmail@example.com>
2026-07-10 15:56:40 +0300

    feat(session-04): Created a new controller class to put the endpoints written using Linq, and implemented the first endpoint (getting a supplier's products)

---
+++ warehouse-management-api-NourM/Warehouse.Presentation/Controllers/DbFirstProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Warehouse.Infrastructure.Models;
+
+namespace Warehouse.Presentation.Controllers;
+
+[ApiController]
+[Route("/dbfirst")]
+public class DbFirstProductsController:ControllerBase
+{
+    private readonly WarehouseDbFirstContext _db;
+
+    public DbFirstProductsController(WarehouseDbFirstContext context)
+    {
+        _db = context;
+    }
+    
+    [HttpGet("bySuppName")]
+    public IActionResult GetProductsBySupplierName([FromQuery] string supplierName,[FromQuery] string order = "desc")
+    {
+        List<Product> products = _db.Products.Where(p=> p.Supplier.Name == supplierName).ToList();
+        if (order == "desc")
+        {
+            products = products.OrderByDescending(p => p.CreatedAt).ToList();
+        }
+        else
+        {
+            products = products.OrderBy(p => p.CreatedAt).ToList();
+        }
+
+        return Ok(products.ToList());
+    }
+    
+}

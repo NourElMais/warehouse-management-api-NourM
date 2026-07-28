a5965cf6e5ce10b20d01cc4a9c3653af10476d2e
Nour <yourEmail@example.com>
2026-07-28 14:11:22 +0300

    feat(session-09): Created the integration test to test the whole application flow

---
+++ warehouse-management-api-NourM/Warehouse.Api.IntegrationTests/IntegrationTests/FullTest.cs
@@ -0,0 +1,116 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Warehouse.Application.ViewModels;
+using Warehouse.Presentation.Contracts;
+
+namespace Warehouse.Api.IntegrationTests.IntegrationTests;
+
+public class FullTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+
+        public FullTest(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+        //helper method that builds the HTTP request that will be sent to the API
+        private static MultipartFormDataContent CreateForm(byte[] bytes, string fileName)
+        {
+            var form = new MultipartFormDataContent();
+
+            var file = new ByteArrayContent(bytes);
+
+            form.Add(file, "image", fileName);
+            return form;
+        }
+
+        [Fact]
+        public async Task FullBusinessFlow_ShouldCompleteSuccessfully()
+        {
+            //1- Create Supplier
+            var supplierRequest = new CreateSupplierRequest
+            {
+                Name = "Rim",
+                Country = "Lebanon",
+                ContactEmail = "Rim@mail.com",
+                PhoneNumber = "81-123456"
+            };
+
+            var supplierResponse = await _client.PostAsJsonAsync("/api/suppliers", supplierRequest);
+            Assert.Equal(HttpStatusCode.OK, supplierResponse.StatusCode);
+            //we get the created supplier's id
+            var supplier = await supplierResponse.Content.ReadFromJsonAsync<SupplierViewModel>();
+            Assert.NotNull(supplier);
+            var supplierId = supplier.Id;
+            
+            //2- Create product
+            var productRequest = new CreateProductRequest
+            {
+                Name = "Ipad",
+                SKU = "ipad123",
+                Description = "Ipad 10 Air",
+                Price = 900,
+                QuantityInStock = 8,
+                SupplierId = "supplier-id1",
+                ExpiryDate = new DateTime(2028, 7, 27)
+            };
+
+            var productResponse = await _client.PostAsJsonAsync("/api/products", productRequest);
+            Assert.Equal(HttpStatusCode.OK, productResponse.StatusCode);
+            var product = await productResponse.Content.ReadFromJsonAsync<ProductViewModel>();
+
+            Assert.NotNull(product);
+
+            var productId = product.Id;
+
+            
+            //3- assign supplier
+            var response = await _client.PostAsync($"/api/products/{productId}/assign-supplier/{supplierId}", null);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var getResponse = await _client.GetAsync($"/api/products/{productId}");
+            Assert.Equal(HttpStatusCode.OK, getResponse.StatusCode);
+            var content = await getResponse.Content.ReadAsStringAsync();
+            Assert.Contains(supplierId, content);
+            
+            // 4- Upload image
+            using var form = CreateForm(new byte[] { 1, 2, 3 }, "ipad.jpg");
+
+            var imageResponse = await _client.PostAsync($"/api/products/{productId}/image", form);
+            Assert.Equal(HttpStatusCode.OK, imageResponse.StatusCode);
+            
+            //5- update quantity 
+            var quantityRequest = new UpdateProductQuantityRequest
+            {
+                QuantityInStock = 20
+            };
+
+            var quantityResponse = await _client.PostAsJsonAsync($"/api/products/{productId}/quantity", quantityRequest);
+            Assert.Equal(HttpStatusCode.OK, quantityResponse.StatusCode);
+            
+            // 6- Update price
+            var priceRequest = new UpdateProductPriceRequest
+            {
+                Price = 1000
+            };
+
+            var priceResponse = await _client.PostAsJsonAsync($"/api/products/{productId}/price", priceRequest);
+            Assert.Equal(HttpStatusCode.OK, priceResponse.StatusCode);
+            
+            
+            // 7. Archive product 
+            var archiveResponse = await _client.DeleteAsync($"/api/products/{productId}");
+            Assert.Equal(HttpStatusCode.OK, archiveResponse.StatusCode);
+
+
+            // 8. verify archived state
+            var res = await _client.GetAsync($"/api/products/{productId}");
+            Assert.Equal(HttpStatusCode.OK, getResponse.StatusCode);
+
+            var archivedProduct = await res.Content.ReadFromJsonAsync<ProductViewModel>();
+
+            Assert.NotNull(archivedProduct);
+            archivedProduct.IsArchived.Should().Be(true);
+
+        }
+    }
